9994c4d23247761c9f6075db023a5209b27057a9
sandbox259 <saadaibani3@gmail.com>
2025-09-26 03:00:19 +0530

    changing colours

--- components/viewer/recent-sales-tab.tsx
+++ components/viewer/recent-sales-tab.tsx
@@ -55,7 +55,7 @@ export default function RecentSalesTab({ assignments }: RecentSalesTabProps) {
       case "Bowler":
         return "bg-gradient-to-r from-red-500 to-red-700"
       case "All Rounder":
-        return "bg-gradient-to-r from-yellow-500 to-green-700"
+        return "bg-gradient-to-r from-green-500 to-green-700"
       case "Wicket Keeper":
         return "bg-gradient-to-r from-purple-500 to-purple-700"
       default:
